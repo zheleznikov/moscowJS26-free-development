103791e499a4231beded38f5a5db42b24cf5772e
Сергей <polwen@yandex.ru>
2026-02-26 22:52:47 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+window.PRESENTATION_CONFIG = {
+    title: 'Что происходит, когда разработчик получает полную свободу?',
+
+    subtitle: 'Сергей Железников',
+
+    // Ссылка в футере
+    badge: {
+        text: 'GitHub',
+        href: 'https://github.com/your-username/your-repo',
+    },
+
+    slides: [
+        '01-title',
+        '02-text-list',
+        '03-columns',
+        '04-code',
+    ],
+};
